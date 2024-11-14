348aa886191550955964d39737e356efe47e437c
Maksudul Arafin <rocksterdowan@gmail.com>
2024-11-14 07:20:07 +0600

    Added 12

--- 12/12.c
+++ 12/12.c
@@ -2,4 +2,56 @@
 Write a program to generate a syntax tree for the sentence  a+b*c with the following
 grammar:
     E→E+E|E-E|E*E|E/E|a|b|c
+
+    Output: E
+            E*E
+            E+E*E
+            a+E*E
+            a+b*E
+            a+b*c
 */
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int len, i;
+    char s[100], str[100] = "E";
+
+    // Read input string
+    scanf("%s", s);
+    len = strlen(s);
+
+    printf("E\n");
+
+    // Loop through the string from the second-last character to the beginning
+    for (i = len - 2; i >= 0; i -= 2)
+    {
+        // Check if the character is an operator
+        if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/')
+        {
+            // Prepend operator and 'E' to str
+            char temp[100] = "E";
+            strncat(temp, &s[i], 1);
+            //char *strncat(char *dest, const char *src, size_t n)
+            //Parameters : This method accepts the following parameters :
+
+            //dest : the string where we want to append.
+            //src : the string from which ‘n’ characters are going to append.
+            //n : represents a maximum number of characters to be appended.size_t is an unsigned integral type.
+            strcat(temp, str);
+            strcpy(str, temp);
+            printf("%s\n", str);
+        }
+    }
+
+    // Loop through even indices and replace characters in str
+    for (i = 0; i < len; i += 2)
+    {
+        str[i] = s[i];
+        printf("%s\n", str);
+    }
+
+    return 0;
+}
