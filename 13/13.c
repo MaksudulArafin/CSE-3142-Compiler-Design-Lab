522b6116b6683eb6f02652131e044f4ae9cdda33
Maksudul Arafin <rocksterdowan@gmail.com>
2024-11-21 10:37:38 +0600

    minor addition

---
+++ 13/13.c
@@ -0,0 +1,11 @@
+/*  13. Write a program which checks a validity of C++ expression derived by the following grammar:  
+    E → E A E | (E) | ID 
+    A → + | - | * | / 
+    ID → any valid identifier | any valid integer 
+    RESULT: 
+    Input: Enter a string: 2+3*5 
+    Output: VALID 
+    Input: Enter a string: 2+*3*5 
+    Output: INVALID 
+ */
+
